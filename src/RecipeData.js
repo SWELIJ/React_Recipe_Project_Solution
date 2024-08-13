892e6ea63e4994df0187ae6abe6679b4ededc624
Elijah Sheridan <151097382+SWELIJ@users.noreply.github.com>
2024-08-13 18:16:47 -0400

    Update pictures for recipes

--- src/RecipeData.js
+++ src/RecipeData.js
@@ -7,14 +7,14 @@ const RecipeData = [
     {
       name: "Tuna Poke with Mango",
       cuisine: "Hawaiian",
-      photo: "https://www.foodista.com/sites/default/files/styles/recype/public/tunapokebowlwithmango22.jpg",
+      photo: "https://media.istockphoto.com/id/1197946481/photo/top-view-of-poke-bowl-with-salmon-on-light-background.jpg?s=612x612&w=0&k=20&c=VCRbK4_aQF2u8GqLvUdmtImhwpiBy43KRRbot0d2uXA=",
       ingredients: "1 package of sushi grade tuna. 1 cup cooked quinoa ½ avocado, sliced. ½ mango, cubed. 1 shredded carrot. 1 small sliced cucumber. poke sauce.",
       preparation: "1. Chop tuna into cubes. 2. Toss with 1 tbsp sesame oil and 1 tbsp tamari. Set aside. 3. Layer your poke bowl starting with quinoa. 4. Whisk all poke sauce ingredients in a bowl and pour over the poke bowl. 5. Garnish with sesame seeds and furikake. 6. Top with chopped scallions."
     },
     {
       name: "Guacamole",
       cuisine: "Mexican",
-      photo: "https://www.foodista.com/sites/default/files/styles/recype/public/017.jpg",
+      photo: "https://media.istockphoto.com/id/1490470359/photo/fresh-guacamole-in-a-wooden-bowl-with-ingredients-in-the-background.jpg?s=612x612&w=0&k=20&c=an1ql-a5Im25hp4FS-LwY9LgDlMzVuTMExAbZcOhGn8=",
       ingredients: "3 ripe avocados. ¾ cup finely chopped Roma tomato. 2 serrano chiles very finely chopped (seeded and deveined). 3 heaping tablespoons of finely chopped onion. 3 tablespoons of minced cilantro. Salt to taste",
       preparation: "1. Remove the flesh of the avocados. 2. Mash the avocados with the back of a fork. 3. Add the other ingredients and incorporate evenly. 4. Add salt to taste."
     },
